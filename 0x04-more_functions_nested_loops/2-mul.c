6b300617e3a6c28b5e0e6daab7916432f8aad021
Kad-19 <kidusasebe1919@example.com>
2023-08-18 04:38:31 -0700

    my first commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digits
+ *@a: a digit
+ *@b: a digit
+ * Return: a mubltiplied by b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+
+#endif
