c54161676bdd8ee68e840f6d57c73aea653e51dd
Kad-19 <kidusasebe1919@example.com>
2023-08-21 14:48:17 -0700

    my third commit corrected aggain

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,13 +9,13 @@ int _atoi(char *s)
 {
 	int sign = 1;
 
-	int result = 0;
+	int res = 0;
 
 	int i = 0;
 
 	while (s[i] == ' ')
 		i++;
-	while (s[i] == '-' || s[i] == '+' || s[i] == ' ')
+	while (s[i] == '-' || s[i] == '+' || s[i] == ' ' || s[i] >= 65)
 	{
 		if (s[i] == '-')
 		{
@@ -25,7 +25,9 @@ int _atoi(char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		result = result * 10 + (s[i++] - '0');
+		if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+			return (sign == -1 ? INT_MIN : INT_MAX);
+		res = res * 10 + (s[i++] - '0');
 	}
-	return (result * sign);
+	return (res * sign);
 }
