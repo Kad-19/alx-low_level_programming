20bf0be3d0d1fc698a7b4f2f89639d1257204381
Kad-19 <kidusasebe1919@example.com>
2023-08-28 13:00:13 -0700

    my second commit corrected

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,28 +4,23 @@
  *_strchr - locates a character in a string
  *@c: a character
  *@s: a pointer to character
- *Return: f
+ *Return: f or 0
  */
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	int j = 0;
-
-	char *f;
-
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
-			while (s[i] != '\0')
-			{
-				f[j] = s[i];
-				j++;
-			}
 			break;
 		}
 		i++;
+		if (s[i] == '\0')
+		{
+			return (0);
+		}
 	}
-	return (f);
+	return (s + i);
 }
