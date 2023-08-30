55e1c67dd9e4fff2850efb0c39156417c54bb343
Kad-19 <kidusasebe1919@example.com>
2023-08-30 04:44:12 -0700

    my first recursion commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *_puts_recursion - prints a string, followed by a new line
+ *@s: a character array
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s != '\0')
+	{
+		_puts_recursion(char *(s + 1));
+	}
+}
