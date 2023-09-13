8e6e137978a822c46b74660d4135e82ec7d66422
Kad-19 <kidusasebe1919@example.com>
2023-09-13 12:15:56 -0700

    my second commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,6 +11,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
 	while (i < size)
 	{
 		if ((*cmp)(array[i]) != 0)
@@ -19,7 +21,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		}
 		i++;
 	}
-	if ((i == size) || (size <= 0))
+	if (i == size)
 		return (-1);
 	return (-1);
 }
