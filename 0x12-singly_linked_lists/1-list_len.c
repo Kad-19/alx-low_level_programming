7b0b926ee408072eba97129d31f15277ec4b2f2b
root <kidusasebe1919@gmail.com>
2023-10-19 13:20:45 -0700

    list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_list - prints members of a list
+ * list_len - returns the number of nodes
  * @h: the pointer to the first node
  * Return: number of nodes
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t nodenum = 0;
 	list_t *curr = (list_t *)h;
 
 	while (curr != NULL)
 	{
-		if (curr->str == NULL)
-		{
-			printf("[0] ");
-			printf("(nil)\n");
-		}
-		else
-		{
-			printf("[%u] ", curr->len);
-			printf("%s\n", curr->str);
-		}
 		curr = curr->next;
 		nodenum++;
 	}
