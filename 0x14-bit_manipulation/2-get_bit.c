b1f4ae0c1bd311d580e2672932fc7e0c4ea1b52f
root <kidusasebe1919@gmail.com>
2023-10-26 05:25:52 -0700

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * last_bit - returns the last it of the binary equivalent
+ * @n: the decimal number
+ * Return: the last bit
+ */
+int last_bit(unsigned long int n)
+{
+	int lz = 1;
+	unsigned long int bin = 1;
+	int bit;
+
+	bin = bin << (sizeof(unsigned long int) * 8 - 1);
+	while (bin > 0)
+	{
+		if (bin & n)
+			lz = 0;
+		if (lz == 0)
+		{
+			if (bin & n)
+				bit = 1;
+			else
+				bit = 0;
+		}
+		bin = bin >> 1;
+	}
+	return (bit);
+}
+/**
+ * get_bit - returns the bit at a specified index
+ * @n: the decimal number
+ * @index: the index of the binary bit
+ * Return: the bit at the specified index or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int temp = n;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (temp > 0)
+	{
+		temp >>= 1;
+		i++;
+	}
+	temp = n;
+	while (j < i)
+	{
+		if (j == index)
+			return (last_bit(temp));
+		temp >>= 1;
+		j++;
+	}
+	return (-1);
+}
+
