5f6adcf33a27aa0e72e5f19db5598909061e23a7
root <kidusasebe1919@gmail.com>
2023-10-26 02:58:07 -0700

    binary to unit correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,24 @@
 #include "main.h"
 /**
+ * powr - calculates the power of a number
+ * @base: the base of the number
+ * @exp: the exponent of the number
+ * Return: the result of the base to the exponet
+ */
+unsigned int powr(int base, int exp)
+{
+	unsigned int num = 1;
+	int i = 0;
+
+	while (i < exp)
+	{
+		num *= base;
+		i++;
+	}
+	return (num);
+}
+
+/**
  * binary_to_uint - converts a binary to an integer
  * @b: a string that contains the binary number
  * Return: the decimal number, 0 if b is NULL or b contains other than 0 or 1
@@ -14,7 +33,7 @@ unsigned int binary_to_uint(const char *b)
 	while (b[i] != '\0')
 	{
 		if (b[i] == '1')
-			num += pow(2, strlen(b) - i - 1);
+			num += powr(2, strlen(b) - i - 1);
 		else if (b[i] != '0')
 			return (0);
 		i++;
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <string.h>
-#include <math.h>
 
 unsigned int binary_to_uint(const char *b);
 
