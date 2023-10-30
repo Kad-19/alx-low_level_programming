7c4d7262e44190a7f6b1c443dd50befad7475fe8
root <kidusasebe1919@gmail.com>
2023-10-30 11:10:58 -0700

    read from file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+/**
+ * read_textfile - reads text from a file and prints it to the stdout
+ * @filename: the name of the file
+ * @letters: number of chars to be printed
+ * Return: 0 or the actual numbers of chars it could print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytesr = 1;
+	ssize_t bytesw;
+	ssize_t total_byte = 0;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	while (bytesr > 0)
+	{
+		bytesr = read(fd, buffer, sizeof(buffer) - 1);
+		if (bytesr == -1)
+		{
+			close(fd);
+			return (0);
+		}
+		total_byte += bytesr;
+		if (total_byte <= (ssize_t)letters)
+		{
+			bytesw = write(STDOUT_FILENO, buffer, bytesr);
+			if (bytesw == -1 || bytesw != bytesr)
+			{
+				close(fd);
+				return (0);
+			}
+		}
+	}
+	close(fd);
+	return (total_byte);
+}
